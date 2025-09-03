02e0f90cc6c474fe121a99dc4730df5c0650c263
Nunu Giuseppin <nunugiuseppin@gmail.com>
2025-09-03 11:24:54 +0000

    Ill provide you the image that i want blened in on the top left of the n

--- src/components/OverviewPage.tsx
+++ src/components/OverviewPage.tsx
@@ -23,7 +23,7 @@ const OverviewPage = ({ userId, overviewPagesData, balances, carouselIndex, hand
       <header className="bg-[#00703C] text-white p-4 flex justify-between items-center w-full flex-shrink-0">
         <div className="flex items-center space-x-2">
           <Image
-            src="https://firebasestorage.googleapis.com/v0/b/van-schalkwyk-trust-mobile.firebasestorage.app/o/logo.png?alt=media&token=0b61c15f-3d8b-4a6c-85f0-6c38a161353c"
+            src="https://i.ibb.co/dKq1gT7/nedbank-logo.png"
             alt="Company Logo"
             width={32}
             height={32}
