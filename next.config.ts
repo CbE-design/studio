ad2193798cc168c29a2849a376e2a81ab412a41b
Nunu Giuseppin <nunugiuseppin@gmail.com>
2025-09-06 21:36:49 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -43,10 +43,6 @@ const nextConfig: NextConfig = {
     ],
   },
   transpilePackages: ['firebase'],
-  experimental: {
-    // This is required to allow requests from the development environment.
-    allowedDevOrigins: ["*.cloudworkstations.dev"],
-  },
 };
 
 export default nextConfig;
