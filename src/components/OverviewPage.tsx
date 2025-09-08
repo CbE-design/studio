89d5096eb9cb4e4f2523e6b931ff583f7f95ce16
Nunu Giuseppin <nunugiuseppin@gmail.com>
2025-09-08 15:22:41 +0000

    Applications: A green line-art icon of a single sheet of paper with the

--- src/components/OverviewPage.tsx
+++ src/components/OverviewPage.tsx
@@ -28,7 +28,28 @@ const OverviewPage = ({ userId, overviewPagesData, balances, carouselIndex, hand
       ),
       text: 'Offers for you'
     },
-    { icon: <FileText size={24} />, text: 'Applications' },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="black"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+          <polyline points="14 2 14 8 20 8"></polyline>
+          <line x1="8" y1="12" x2="16" y2="12"></line>
+          <line x1="8" y1="15" x2="16" y2="15"></line>
+          <line x1="8" y1="18" x2="12" y2="18"></line>
+        </svg>
+      ),
+      text: 'Applications'
+    },
     { icon: <Umbrella size={24} />, text: 'Insure', badge: 5 },
     { icon: <Car size={24} />, text: 'Discs and fines' },
     { icon: <ShoppingCart size={24} />, text: 'Shop', badge: 1 },
