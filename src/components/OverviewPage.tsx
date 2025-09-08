2e88da41c46b08d609edaf53187df301be67426e
Nunu Giuseppin <nunugiuseppin@gmail.com>
2025-09-08 13:14:35 +0000

    Please restore the colour code to the previous on the accounts screen an

--- src/components/OverviewPage.tsx
+++ src/components/OverviewPage.tsx
@@ -48,7 +48,7 @@ const OverviewPage = ({ userId, overviewPagesData, balances, carouselIndex, hand
 
   return (
     <div className="flex-1 flex flex-col overflow-hidden">
-      <div className="bg-gradient-to-b from-[#34D399] to-[#059669]">
+      <div className="bg-gradient-to-b from-[#009E4D] to-[#008D41]">
         <header className="bg-transparent text-primary-foreground p-4 flex justify-between items-center w-full flex-shrink-0">
           <div className="flex items-center space-x-4">
             <Image
