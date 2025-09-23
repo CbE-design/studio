5216b21c678daa173e5abafc8c02597448f46026
Nunu Giuseppin <nunugiuseppin@gmail.com>
2025-09-23 08:43:15 +0000

    Use the image attached as the web apps logo on the phone screen when dow

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,27 +4,15 @@ export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("flex items-center justify-center w-12 h-12 rounded-lg bg-white", className)}>
        <svg
-        className="w-8 h-8 text-primary"
-        viewBox="0 0 50 50"
+        className="w-8 h-8"
+        viewBox="0 0 100 100"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path
-          d="M25.0002 0L50 14.4338V43.3013L25.0002 57.735L0 43.3013V14.4338L25.0002 0Z"
-          fill="#00573D"
-        ></path>
-        <path
-          d="M25.0002 4.81122L45.1925 16.8524V40.8825L25.0002 52.9238L4.80791 40.8825V16.8524L25.0002 4.81122Z"
-          fill="#009C6D"
-        ></path>
-        <path
-          d="M25 9.62244L4.80771 21.6636V36.0713L25 48.1125L45.1923 36.0713V21.6636L25 9.62244Z"
-          fill="#25A95A"
-        ></path>
-        <path
-          d="M25.0002 14.4338L45.1925 26.475V31.2862L25.0002 43.3274L4.80791 31.2862V26.475L25.0002 14.4338Z"
-          fill="#FFFFFF"
-        ></path>
+        <rect width="100" height="100" fill="#008A52"/>
+        <path d="M62.5 12.5L87.5 27.5V72.5L62.5 87.5H37.5L12.5 72.5V27.5L37.5 12.5H62.5Z" fill="#00573D"/>
+        <path d="M81.25 29.375V70.625L60.625 81.25H39.375L18.75 70.625V29.375L39.375 18.75H60.625L81.25 29.375Z" stroke="white" stroke-width="6.25"/>
+        <path d="M60.625 18.75L39.375 43.75V81.25L60.625 56.25V18.75Z" fill="white"/>
       </svg>
     </div>
   );
