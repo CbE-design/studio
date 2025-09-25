cb047111ac5de878c223ce6644e40927463333d1
Nunu Giuseppin <nunugiuseppin@gmail.com>
2025-09-25 14:30:53 +0000

    Move Corrie more to the right away from the N logo in the top header

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -261,7 +261,7 @@ export default async function DashboardPage() {
       {/* Header */}
       <header className="bg-primary text-primary-foreground p-4 space-y-4 sticky top-0 z-10">
         <div className="flex items-center justify-between">
-          <div className="flex items-center gap-5">
+          <div className="flex items-center gap-6">
             <Image 
               src="https://firebasestorage.googleapis.com/v0/b/studio-3883937532-b7f00.firebasestorage.app/o/NED.JO.png?alt=media&token=990d35fb-2ebf-42c4-988e-78999a4e09d7" 
               alt="Nedbank Logo"
