6547169bd9b035d0fa663f16612274c399cfb4a4
Nunu Giuseppin <nunugiuseppin@gmail.com>
2025-09-25 19:32:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3883937532-b7f00",
+  "appId": "1:653581985755:web:e57973f0b7a6f2436fe9fe",
+  "apiKey": "AIzaSyBHWld1vQcm_cnfYGmd3Nz0COppTheVtGw",
+  "authDomain": "studio-3883937532-b7f00.firebaseapp.com",
+  "measurementId": "G-58GYN4ZKBZ",
+  "messagingSenderId": "653581985755"
+};
