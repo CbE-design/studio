7d2d66e2ae9683f9315e3b8ad7dae628f89ae94c
Nunu Giuseppin <nunugiuseppin@gmail.com>
2025-09-28 14:22:05 +0000

    The lines must be black of colour on the proof of payment pdf implemente

--- src/app/(app)/pay/single/share/page.tsx
+++ src/app/(app)/pay/single/share/page.tsx
@@ -89,7 +89,7 @@ function ShareProofOfPaymentContent() {
             height: logoDims.height,
         });
 
-        page.drawLine({ start: { x: margin, y: lineY }, end: { x: width - margin, y: lineY }, thickness: 2, color: rgb(0.6, 0.6, 0.6) });
+        page.drawLine({ start: { x: margin, y: lineY }, end: { x: width - margin, y: lineY }, thickness: 2, color: rgb(0, 0, 0) });
         let y = lineY - 25;
 
         page.drawText('Notification of Payment', { x: margin, y, font: boldFont, size: 12, color: textColor });
@@ -159,7 +159,7 @@ function ShareProofOfPaymentContent() {
 
         y = drawWrappedText('Nedbank will never send you an e-mail link to access Verify payments, always go to Online Banking on www.nedbank.co.za and click on Verify payments.', { ...commonTextOptions, x: margin, y });
         y -= 5;
-        page.drawLine({ start: { x: margin, y: y }, end: { x: width - margin, y: y }, thickness: 1, color: rgb(0.6, 0.6, 0.6) });
+        page.drawLine({ start: { x: margin, y: y }, end: { x: width - margin, y: y }, thickness: 1, color: rgb(0, 0, 0) });
         y -= 15;
 
         y = drawWrappedText('This notification of payment is sent to you by Nedbank Limited Reg No 1951/000009/06. Enquiries regarding this payment notification should be directed to the Nedbank Contact Centre on 0860 555 111. Please contact the payer for enquiries regarding the contents of this notification. Nedbank Ltd will not be held responsible for the accuracy of the information on this notification and we accept no liability whatsoever arising from the transmission and use of the information. Payments may take up to three business days. Please check your account to verify the existence of the funds.', { ...commonTextOptions, x: margin, y });
