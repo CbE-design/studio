4b526099f265795b670ab46ee8fd73fe0f5fd780
Nunu Giuseppin <nunugiuseppin@gmail.com>
2025-09-28 18:44:21 +0000

    Can you implement this image attached as the logo for the web app if dow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { FirebaseProvider } from '@/firebase-provider';
 export const metadata: Metadata = {
   title: 'MoneyGO',
   description: 'Your trusted banking partner.',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -16,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head />
+      <head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="apple-touch-icon" href="https://storage.googleapis.com/fpa-users-files-prod/prompt_images/8051759654719080061/a6064972-e19c-46cc-b333-e9686036f014.png" />
+      </head>
       <body className={cn("antialiased")}>
         <FirebaseProvider>
           {children}
