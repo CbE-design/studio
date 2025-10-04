47feec062d41b88ed3e14f6a02c7180057ff90c1
Nunu Giuseppin <nunugiuseppin@gmail.com>
2025-10-04 11:04:17 +0000

    https://firebasestorage.googleapis.com/v0/b/studio-3883937532-b7f00.fire

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -70,7 +70,7 @@ export default function LoginPage() {
 
       <main className="flex-1 overflow-y-auto px-6 py-8 flex flex-col items-center text-center">
         <Avatar className="h-20 w-20 mb-4">
-            <AvatarImage src="https://i.pravatar.cc/150?u=a042581f4e29026704d" alt="User Avatar" />
+            <AvatarImage src="https://firebasestorage.googleapis.com/v0/b/studio-3883937532-b7f00.firebasestorage.app/o/My%20Widget%20Buttons%2FIMG_20251004_130049.jpg?alt=media&token=6d303043-9f10-4721-8444-cc62a1009dc9" alt="User Avatar" />
             <AvatarFallback>VS</AvatarFallback>
         </Avatar>
         <h1 className="text-xl font-semibold">Van Schalkwyk Family Trust</h1>
