ab8c30ec48b423ba74fc415fae52f07ebc5a128f
Nunu Giuseppin <nunugiuseppin@gmail.com>
2026-02-15 19:47:52 +0000

    the paragraphs is still not like i want it to be

--- src/lib/pop-generator.ts
+++ src/lib/pop-generator.ts
@@ -165,10 +165,15 @@ export async function generateProofOfPaymentPdf(transaction: Transaction, accoun
     page.drawLine({ start: { x: margin, y: y }, end: { x: width - margin, y: y }, thickness: 1, color: rgb(0, 0, 0) });
     y -= 20;
 
-    const disclaimerText = 'This notification of payment is sent to you by Nedbank Limited Reg No 1951/000009/06. Enquiries regarding this payment notification should be directed to the Nedbank Contact Centre on 0860 555 111. Please contact the payer for enquiries regarding the contents of this notification. Nedbank Ltd will not be held responsible for the accuracy of the information on this notification and we accept no liability whatsoever arising from the transmission and use of the information. Payments may take up to three business days. Please check your account to verify the existence of the funds.';
-    
-    y = drawWrappedText(disclaimerText, { ...commonTextOptions, x: margin, y });
-    y -= commonTextOptions.lineHeight;
+    const disclaimerParagraphs = [
+        'This notification of payment is sent to you by Nedbank Limited Reg No 1951/000009/06. Enquiries regarding this payment notification should be directed to the Nedbank Contact Centre on 0860 555 111. Please contact the payer for enquiries regarding the contents of this notification. Nedbank Ltd will not be held responsible for the accuracy of the information on this notification and we accept no liability whatsoever arising from the transmission and use of the information.',
+        'Payments may take up to three business days. Please check your account to verify the existence of the funds.'
+    ];
+
+    disclaimerParagraphs.forEach(paragraph => {
+        y = drawWrappedText(paragraph, { ...commonTextOptions, x: margin, y });
+        y -= commonTextOptions.lineHeight;
+    });
     
     y -= 5;
     
