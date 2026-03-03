b213569465cf7581c5ffdc2c5a415c571a44e125
Nunu Giuseppin <nunugiuseppin@gmail.com>
2026-03-03 19:26:49 +0000

    https://firebasestorage.googleapis.com/v0/b/studio-3883937532-b7f00.fire

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -255,7 +255,7 @@ export default function DashboardPage() {
         <div className="bg-white flex-1 pb-4">
           <div className="mx-auto w-[calc(100%-2rem)] max-w-lg overflow-hidden rounded-md shadow-sm border border-black/20 mt-4 bg-white">
             <Image
-              src="https://firebasestorage.googleapis.com/v0/b/studio-3883937532-b7f00.firebasestorage.app/o/IMG_20260303_210333.jpg?alt=media"
+              src="https://firebasestorage.googleapis.com/v0/b/studio-3883937532-b7f00.firebasestorage.app/o/IMG_20260303_210333.jpg?alt=media&token=bfc49ba7-9c39-41aa-a85b-b7b2a3ec9dc0"
               alt="Advertisement banner"
               data-ai-hint="advertisement banner"
               width={600}
