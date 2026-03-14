332e779c6d1c821bdac971955182e78114256f2a
Nunu Giuseppin <nunugiuseppin@gmail.com>
2026-03-14 23:29:18 +0000

    the top section of the gradient must rather be a lighter dark lime green

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: '#004033',
+  themeColor: '#008248',
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
