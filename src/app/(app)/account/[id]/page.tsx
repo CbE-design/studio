ccf3075bfc6f801a11e85f1295b0a6fc7b50cc33
Nunu Giuseppin <nunugiuseppin@gmail.com>
2026-03-20 12:23:02 +0000

    https://firebasestorage.googleapis.com/v0/b/studio-3883937532-b7f00.fire

--- src/app/(app)/account/[id]/page.tsx
+++ src/app/(app)/account/[id]/page.tsx
@@ -203,7 +203,7 @@ export default function AccountDetailsPage() {
               <Link href="/notifications">
                 <div className="relative w-6 h-6 bg-[#2D5A27]">
                   <Image
-                    src="https://firebasestorage.googleapis.com/v0/b/studio-3883937532-b7f00.firebasestorage.app/o/20260320141309.png?alt=media"
+                    src="https://firebasestorage.googleapis.com/v0/b/studio-3883937532-b7f00.firebasestorage.app/o/20260320141309.png?alt=media&token=1836ae99-d919-48db-85fe-013baef40979"
                     alt="Notifications"
                     fill
                     className="object-contain"
